a877105a5f87a70a7ca030f96ee5d39dd5da4981
김현석 <dragmove@naver.com>
2018-01-18 18:17:21 +0900

    Add jquery in devDependencies

--- app/src/kihon.js
+++ app/src/kihon.js
@@ -1,9 +1,4 @@
 import FullSizeBg from './component/FullSizeBg';
 import Overlay from './component/Overlay';
 
-const Kihon = {
-  FullSizeBg,
-  Overlay
-};
-
-export default Kihon;
+export {FullSizeBg, Overlay};
